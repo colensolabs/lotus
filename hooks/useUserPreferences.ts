42302292083d76f03f95c48c8e610d5c33c59b9f
colensolabs <colensolabs@gmail.com>
2025-08-16 15:24:17 +0100

    Fix user preferences query error

--- hooks/useUserPreferences.ts
+++ hooks/useUserPreferences.ts
@@ -64,9 +64,9 @@ export const useUserPreferences = () => {
         .from('user_preferences')
         .select('*')
         .eq('user_id', user.id)
-        .single();
+        .maybeSingle();
 
-      if (error && error.code !== 'PGRST116') { // PGRST116 = no rows returned
+      if (error) {
         throw error;
       }
 
